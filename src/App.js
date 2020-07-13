a77251434b9fbf2636595004c8a8d75b97a1666c
Arsi-Air <32060641+Arsi-Air@users.noreply.github.com>
2020-07-13 22:38:16 +0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import imgLogo from './logo.png';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
 
 import { Shoes } from './components/Shoes/Shoes';
 import { Home } from './components/Home/Home';
-import {  } from './components/LaunchIndex/LaunchIndex';
 import { NotFound } from './components/NotFound/NotFound';
 
 
@@ -15,7 +15,8 @@ function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>
+      <img className="imgLogo" src={imgLogo} alt="Arsi logo" />
+        <Link to="/" className="navHome" >Home</Link>
         <Link to="/launch">Products</Link>
         sdsdsda
       </nav>
@@ -47,12 +48,15 @@ const LaunchIndex = () => {
     <div className="wrapperLI">
     <p className="runningTitle">Running Shoes</p>
     <ul>
-      {Object.entries(Shoes).map(([slug, { name, img, price }]) => (
+      {Object.entries(Shoes).map(([slug, { name, img, price, stock }]) => (
         <li className="launchShoes" key={slug}>
           <Link to={`/launch/${slug}`}>
             <img className="launchPics" src={img} alt={name} />
             <h3 className="launchHeader">{name}</h3>
+            <div className="underImage">
+            <p className="stock">{stock} left in stock</p>
             <p className="price">USD {price}</p>
+            </div>
           </Link>
         </li>
       ))}
@@ -71,12 +75,25 @@ const LaunchShoe = () => {
     
   }
 
-  const { name, img } = shoe;
+  const { name, img, img2 } = shoe;
 
   return (
     <div>
-      <h2>{name}</h2>
-      <img src={img} alt={name} />
+      <h2 className="prodHeader">{name}</h2>
+      {/* <table className="prodDescTable" >
+      <tr> */}
+      {/* <td> */}
+      <div className="prodDivMain">
+      <div className="prodDiv">
+      <img className="prodPageImg" src={img} alt={name} />
+      {/* </td> */}
+      {/* <td> */}
+      <img className="prodPageImg" src={img2} alt={name} />
+      </div>
+      </div>
+      {/* </td> */}
+      {/* </tr>
+      </table> */}
       </div>
   );
 }
--- src/components/Shoes/Shoes.js
+++ src/components/Shoes/Shoes.js
@@ -1,5 +1,3 @@
-import img1 from './zoom-rival-m-8-distance-spike.png'
-
 
 export const Shoes = {
   "nike-air-zoom-pegasus": {
@@ -7,55 +5,71 @@ export const Shoes = {
     img:
       "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/33201b0e-8971-45eb-b91b-edac41535f5a/air-zoom-pegasus-37-running-shoe-Z7QV8F.jpg",
     site: "https://www.nike.com/ae/t/air-zoom-pegasus-37-running-shoe-Z7QV8F/BQ9646-001",
-    price: 149
+    price: 149,
+    stock: 4,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/2ef0a989-2bb6-41cc-8c0a-6b2640bf117c/air-zoom-pegasus-37-running-shoe-Z7QV8F.jpg"
   },
   "nike-revolution-5-flyease": {
     name: "Nike Revolution 5 FlyEase",
     img:
       "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/kcjjenjhdfbpm8on4rqs/revolution-5-flyease-running-shoe-Jdfmdm.jpg",
     site: "https://www.nike.com/ae/t/revolution-5-flyease-running-shoe-Jdfmdm/CJ9885-600",
-    price: 76
+    price: 76,
+    stock: 8,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/i2hcfq1odysvfgknvvlq/revolution-5-flyease-running-shoe-Jdfmdm.jpg"
   },
   "nike-todos-rn": {
     name: "Todos RN",
     img:
       "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/tkb1v3fchkv5l4nqrt2m/todos-rn-running-shoe-FzZPPJ.jpg",
     site: "https://www.nike.com/ae/t/todos-rn-running-shoe-FzZPPJ/BQ3198-001",
-    price: 73
+    price: 73,
+    stock: 12,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/hpuoplcbptla7lilnrfe/todos-rn-running-shoe-FzZPPJ.jpg"
   },
-  "nike-react-phantom-run-flyknit-2": {
-    name: "React Phantom Run Flyknit 2",
+  "nike-phantom-run-flyknit-2": {
+    name: "Phantom Run Flyknit 2",
     img:
       "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/f21ce99a-a0f1-410f-a2ff-c93b27f457b2/react-phantom-run-flyknit-2-running-shoe-Djlst6.jpg",
     site: "https://www.nike.com/ae/t/react-phantom-run-flyknit-2-running-shoe-Djlst6/CJ0277-003",
-    price: 177
+    price: 177,
+    stock: 7,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/1eeaeea6-1449-4760-a05a-2b72aabcd7da/react-phantom-run-flyknit-2-running-shoe-Djlst6.jpg"
   },
   "nike-react-Infinity-Run-Flyknit": {
     name: "React Infinity Run Flyknit",
     img:
       "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/05c38a6c-3059-487f-8143-1b73344fdfbb/react-infinity-run-flyknit-running-shoe-WMtrNz.jpg",
     site: "https://www.nike.com/ae/t/react-infinity-run-flyknit-running-shoe-WMtrNz/CW7597-100",
-    price: 199
+    price: 199,
+    stock: 2,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/6c21f0ee-7445-4bc9-a5c1-bfd9f8feae09/react-infinity-run-flyknit-running-shoe-WMtrNz.jpg"
   },
-  "nike-zoom-rival-m": {
-    name: "Zoom Rival M 8",
+  "nike-air-zoom-structure-22": {
+    name: "Nike Air Zoom Structure 22",
     img:
-      img1,
-    site: "https://www.nike.com/ae/t/zoom-rival-m-8-distance-spike-89Tqzb9p/806555-017",
-    price: 95
+      "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/i1-be913c2b-6a14-44a1-95e9-043ba3a55b23/air-zoom-structure-22-running-shoe-1XVRql.jpg",
+    site: "https://www.nike.com/ae/t/air-zoom-structure-22-running-shoe-1XVRql/CW2641-001",
+    price: 149,
+    stock: 9,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/i1-6b0e1759-a210-4162-a963-cdf9d233117c/air-zoom-structure-22-running-shoe-1XVRql.jpg"
   },
   "nike-downshifter-10": {
     name: "Nike Downshifter 10",
     img:
       "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/71d73792-f98d-4461-9700-32381e1002d7/downshifter-10-running-shoe-QL0NBl.jpg",
     site: "https://www.nike.com/ae/t/downshifter-10-running-shoe-QL0NBl/CI9981-400",
-    price: 80
+    price: 80,
+    stock: 4,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/da83cbf1-20af-47ab-844e-15db3c40a36b/downshifter-10-running-shoe-QL0NBl.jpg"
   },
   "nike-pegasus-trail-2": {
     name: "Nike Pegasus Trail 2",
     img:
       "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/cf42fe30-7c78-45b5-87bb-8ed11429faec/pegasus-trail-2-trail-running-shoe-ZCfbql.jpg",
     site: "https://www.nike.com/ae/t/pegasus-trail-2-trail-running-shoe-ZCfbql/CK4305-700",
-    price: 163
+    price: 163,
+    stock: 6,
+    img2: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/3ecb7f95-263e-4620-875e-ce46ad5b4daa/pegasus-trail-2-trail-running-shoe-ZCfbql.jpg"
   }
 };
